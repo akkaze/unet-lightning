e1fe0c748a2fe157b34866fce84f46586e745108
Hiep Pham <hiepph.2406@gmail.com>
2019-12-02 11:01:46 +0000

    Unet: basic implementation

---
+++ Unet.py
@@ -0,0 +1,89 @@
+import os
+import logging
+from argparse import ArgumentParser
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch import optim
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+from torchvision.datasets import MNIST
+
+import pytorch_lightning as pl
+from pytorch_lightning.core.lightning import LightningModule
+
+
+class Unet(pl.LightningModule):
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(Unet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        def double_conv(in_channels, out_channels):
+            return nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+            )
+
+        def down(in_channels, out_channels):
+            return nn.Sequential(
+                nn.MaxPool2d(2),
+                double_conv(in_channels, out_channels)
+            )
+
+        def up(nn.Module):
+            def __init__(self, in_channels, out_channels):
+                if bilinear:
+                    self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+                else:
+                    self.up = nn .Upsample(in_channels // 2,
+                                           in_channels // 2,
+                                           kernel_size=2, stride=2)
+
+                self.conv = double_conv(in_channels, out_channels)
+
+            def forward(self, x1, x2):
+                x1 = self.up(x1)
+                # [?, C, H, W]
+                diffY = x2.size()[2] - x1.size()[2]
+                diffX = x2.size()[3] - x1.size()[3]
+                print(x1.size())
+                print(x2.size())
+
+
+                x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                                diffY // 2, diffY - diffY // 2])
+                x = torch.cat([x2, x1], dim=1) ## why 1?
+                print(x.size())
+                return self.conv(x)
+
+        self.inc = double_conv(n_channels, 64)
+        self.down1 = down(64, 128)
+        self.down2 = down(128, 256)
+        self.down3 = down(256, 512)
+        self.down4 = down(512, 512)
+        self.up1 = up(1024, 256)
+        self.up2 = up(512, 128)
+        self.up3 = up(256, 64)
+        self.up4 = up(128, 64)
+        self.out = nn.Conv2d(64, n_classes, kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        return self.out(x)
