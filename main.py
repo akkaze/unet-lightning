315c86d0a12abcf00be1f489ff6b1d9be4b095b6
Hiep Pham <hiepph.2406@gmail.com>
2019-12-06 04:35:16 +0000

    main: checkpoint callback

--- main.py
+++ main.py
@@ -6,13 +6,29 @@ import torch
 
 from Unet import Unet
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.logging import TestTubeLogger
 
 
 def main(hparams):
     model = Unet(hparams)
 
+    checkpoint_callback = ModelCheckpoint(
+        filepath=os.getcwd(),
+        save_best_only=False,
+        verbose=True,
+        monitor='loss',
+        mode='min',
+        prefix='unet-'
+    )
+    logger = TestTubeLogger(
+        save_dir='./lightning_logs',
+        version=1,
+    )
     trainer = Trainer(
         gpus=1,
+        checkpoint_callback=checkpoint_callback,
+        logger=logger,
     )
 
     trainer.fit(model)
